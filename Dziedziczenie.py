6edc79b0dced306a5c72dafa5cfa281ce71da35b
Piotr Gadnicki <piotrgadnicki@gmail.com>
2016-01-06 15:55:05 +0100

    blabla

---
+++ Dziedziczenie.py
@@ -0,0 +1,19 @@
+class A:
+    def __str__(self):
+        return "Jestem wesolym obiektem klasy A"
+
+    def __len__(self):
+        return 6
+
+    def __gt__(self, other): #metoda zwracajaca wartosc logiczna  PRZECIAZENIE OPERATORA
+        return True
+
+class B(A):
+    def __init__(self):
+        A.__init__()
+    def __len__(self):
+        return 7
+
+b = B()
+print(b)
+""" zmiany lub nie"""
